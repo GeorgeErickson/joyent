7717a5a1cba6414a41b24623245fe487d1bc0bc8
George Erickson <george55@mit.edu>
2011-07-07 13:59:33 -0400

    testing connect middleware

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 var http = require('http');
 var connect = require('connect');
 var app = connect.createServer()
-  //.use(connect.favicon())
-  //.use(connect.logger())
-  //.use(connect.static(__dirname + '/public'))
+  .use(connect.favicon())
+  .use(connect.logger())
+  .use(connect.static(__dirname + '/public'))
   .listen(process.env.PORT || 8001);
  
 connect.router(function(app){
